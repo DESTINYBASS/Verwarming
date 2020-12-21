21051b1e92f0f6d74fcb9a17e69531f64809c667
Emile Deloof <deloofemile@gmail.com>
2020-12-21 20:00:18 +0100

    modified:   routes/articles.js 	modified:   server.js 	modified:   views/kamers/index.ejs 	modified:   views/kamers/show.ejs

--- routes/articles.js
+++ routes/articles.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const Article = require("./../models/article");
 const router = express.Router();
+const { spawn } = require("child_process");
 
 router.get("/new", (req, res) => {
     res.render("kamers/new", {article: new Article() })
@@ -14,6 +15,7 @@ router.get("/edit/:id", async (req, res) =>{
 router.get("/:slug", async (req, res) => {
     const article = await Article.findOne({ slug: req.params.slug})
     if(article == null) res.redirect("/")
+    getPython();
     res.render("kamers/show", { article: article })
 })
 
@@ -33,14 +35,39 @@ router.delete("/:id", async (req, res) => {
 })
 
 router.put("/:id", async (req, res, next) => {
-    req.article = await Article.findById(req.params.id)
-    next()
+    req.article = await Article.findById(req.params.id);
+    next();
 }, saveArticleAndRedirect("show"))
+
+function getPython(){
+    var dataToSend;
+    const python = spawn('python', ['script1.py']);
+
+    python.stdout.on('data', function (data) {
+        dataToSend = data.toString();
+    });
+
+    python.on('close', (code) => {
+        dataToSend = dataToSend.split(",");
+        let emptyArray = []
+        dataToSend.forEach(el => {
+            try{
+                let newInt = parseInt(el);
+                if(!Number.isNaN(newInt)){
+                    emptyArray.push(newInt);
+                }
+            } catch (e){
+                console.log(e);
+            }
+        });
+    })
+}
 
 function saveArticleAndRedirect(path){
     return async (req, res) => {
         let article = req.article
         article.title = req.body.title
+        article.pastTemp = req.body.emptyArray
         try {
             article = await article.save()
             res.redirect("/")
--- server.js
+++ server.js
@@ -2,60 +2,19 @@ if(process.env.NODE_ENV !== "production"){
     require("dotenv").config();
 }
 
-const { spawn } = require("child_process");
 const express = require("express");
 const mongoose = require("mongoose");
 const articleRouter = require("./routes/articles");
 const methodOverride = require("method-override");
 const Article = require("./models/article");
-const article = require("./models/article");
 const app = express();
 const PORT = process.env.PORT || 5001;
-const router = express.Router()
 
 mongoose.connect(process.env.DATABASE_URL, {
     useNewUrlParser: true, 
     useUnifiedTopology: true,
     useCreateIndex: true
 });
-
-function getPython(){
-    var dataToSend;
-    const python = spawn('python', ['script1.py']);
-
-    python.stdout.on('data', function (data) {
-        dataToSend = data.toString();
-    });
-
-    python.on('close', (code) => {
-        router.put("/", async (req, res, next) => {
-            dataToSend = dataToSend.split(",");
-            let emptyArray = []
-            dataToSend.forEach(el => {
-                try{
-                    let newInt = parseInt(el);
-                    if(!Number.isNaN(newInt)){
-                        article.pastTemp.push(newInt);
-                    }
-                } catch (e){
-                    console.log(e);
-                }
-            });
-            req.article = await Article.findById(req.params.id);
-            return async (req, res) => {
-                let article = req.article
-                article.pastTemp = emptyArray
-                try{
-                    article = await article.save()
-                    res.redirect("/")
-                } catch(e){
-                    res.render("/kamers", { article: article })
-                    console.log(e)
-                }
-            }
-        })
-    });
-}
 
 app.set("view engine", "ejs");
 
@@ -65,7 +24,6 @@ app.use(methodOverride("_method"));
 
 app.get("/", async (req, res) => {
     const articles = await Article.find().sort({ createdAt: "desc"});
-    getPython();
     res.render("kamers/index", { articles: articles});
 })
 
